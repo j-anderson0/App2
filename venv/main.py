5e918197728378a4bb819fcd7bce1290c1729692
seraphJ <j.anderson822@gmail.com>
2024-01-01 21:52:03 -0500

    Updated to me, actually

--- venv/main.py
+++ venv/main.py
@@ -1,12 +1,14 @@
 import streamlit as st 
 
-col1, col2 = st.colums(2)
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/replacement_photo.tiff", width=700)
     
 with col2:
-    st.title("J Elder")
+    st.title("J Mathias")
     content = """Hi! I'm J,humble student of the world. I'm a data scientist and a budding software engineer. 
     I'm passionate about using data to solve problems and build products that make life easier. 
     I'm a self-taught programmer, and I'm always looking for opportunities to learn and grow. 
