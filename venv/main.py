14263b898c3de8a0b6573b0e6e44c6ca99cbe58e
seraphJ <j.anderson822@gmail.com>
2024-01-01 21:02:28 -0500

    Main setup page

---
+++ venv/main.py
@@ -0,0 +1,16 @@
+import streamlit as st 
+
+col1, col2 = st.colums(2)
+
+with col1:
+    st.image("images/photo.png")
+    
+with col2:
+    st.title("J Elder")
+    content = """Hi! I'm J,humble student of the world. I'm a data scientist and a budding software engineer. 
+    I'm passionate about using data to solve problems and build products that make life easier. 
+    I'm a self-taught programmer, and I'm always looking for opportunities to learn and grow. 
+    I'm currently looking for a job as a data scientist or software engineer. 
+    If you're looking for a passionate, hard-working, and dedicated person to join your team, please reach out to me. I'd love to hear from you!
+    """
+    st.write(content)
