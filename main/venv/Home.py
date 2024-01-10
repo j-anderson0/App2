bbaff4c6e48d00683b770e85ae8c0f28842843c4
seraphJ <j.anderson822@gmail.com>
2024-01-09 21:31:10 -0500

    Minor changes

--- main/venv/Home.py
+++ main/venv/Home.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/replacement_photo.tiff", width=600)
+    st.image("images/replacement_photo.tiff", width=300)
 
 with col2:
     st.title("J Mathias")
@@ -17,13 +17,13 @@ with col2:
     I'm a self-taught programmer, and I'm always looking for opportunities to learn and grow. 
     I'm currently looking for a job as a data scientist or software engineer. 
     If you're looking for a passionate, hard-working, and dedicated person to join your team, please reach out to me. 
-    I'd love to hear from you! <br><br> <br><br>
+    I'd love to hear from you! <br><br><br>
     </div>
     """
     st.markdown(content, unsafe_allow_html=True)
     
     content2 = """
-    <div style='text-align: justify; font-size: 40px;'>
+    <div style='text-align: justify; font-size: 30px;'>
     Below you can find some of the apps I have built in Python. 
     Feel free to contact me!
     </div>
