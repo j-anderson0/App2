586a4452d1be4a7e9f4642dd4ceb6519a4fb3e53
seraphJ <j.anderson822@gmail.com>
2024-01-09 21:33:10 -0500

    Minor updated

--- main/venv/send_email.py
+++ main/venv/send_email.py
@@ -1,25 +1,16 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "seraphj51@gmail.com"
-password = "ovnf lgov qwis xzvd"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "j.anderson822@gmail.com"
-context = ssl.create_default_context()
+    username = "seraphj51@gmail.com"
+    password = "ovnf lgov qwis xzvd"
 
-message = """Subject: Python emailing test
-Holla 'atcha boy!
+    receiver = "j.anderson822@gmail.com"
+    context = ssl.create_default_context()
 
-The path forward is only forward and upward, my friend.
-
-Sincerely,
-
-J
-"""
-
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
