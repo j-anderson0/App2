d008d75d1451524b50a3337d82437a30168adc6b
seraphJ <j.anderson822@gmail.com>
2024-01-11 22:02:22 -0500

    Updated to format requested

--- Website example/pages/Contact_us.py
+++ Website example/pages/Contact_us.py
@@ -8,13 +8,12 @@ with st.form(key='email_form'):
     topic = st.selectbox(label='What topic do you want to discuss?', options=['Job Inquiry', 'Project Proposals', 'Other'])
     text = st.text_area(label='Text')
     text = f"""\
-Subject: {name} has sent you a message
-
 From: {email}
-{message}
+{topic}
+{text}
 """
     submit_button = st.form_submit_button(label='Submit')
     print(submit_button)
     if submit_button:
-        send_email(message)
+        send_email(text)
         st.info("Your email was sent successfully!")
