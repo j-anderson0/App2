f772544c0f5098de1cb78e83f5f6712a446b35a6
seraphJ <j.anderson822@gmail.com>
2024-01-11 22:01:54 -0500

    Initial commit - uploaded function for email

---
+++ Website example/send_email.py
@@ -0,0 +1,16 @@
+import smtplib, ssl
+
+
+def send_email(text):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "seraphj51@gmail.com"
+    password = "ovnf lgov qwis xzvd"
+
+    receiver = "j.anderson822@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, text)
